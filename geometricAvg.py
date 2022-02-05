41f6662c2582ea531d54545ef7580ea67b0eaa1d
Julen <39666887+jparrai@users.noreply.github.com>
2022-02-05 09:52:17 +0100

    Create geometricAvg.py
    # Class for defining an aggregate function to calculate 
    # the geometric average of the selected values in a sqlite db
    # Will discard all NULL or zero values, there are other ways of treating this values
    # Will return None if no values or the geometric mean of the non-zero values
    # USAGE: sqliteConnection.create_aggregate("g_avg", 1, GeometricAvg)
    #        cursor.execute("SELECT g_avg(value) FROM data')

---
+++ geometricAvg.py
@@ -0,0 +1,23 @@
+# Class for defining an aggregate function to calculate 
+# the geometric average of the selected values in a sqlite db
+# Will discard all NULL or zero values, there are other ways of treating this values
+# Will return None if no values or the geometric mean of the non-zero values
+# USAGE: sqliteConnection.create_aggregate("g_avg", 1, GeometricAvg)
+#        cursor.execute("SELECT g_avg(value) FROM data')
+
+class GeometricAvg:
+    def __init__(self):
+        self.counter = 0
+        self.mean = 0.0
+
+    def step(self, value):
+        if type(value) == int or type(value) == float:
+            if value is not None and value > 0:
+                self.mean += np.log(value)
+                self.counter += 1
+
+    def finalize(self):
+        if self.counter > 0:
+            return np.exp(self.mean / self.counter)
+        else:
+            return None
